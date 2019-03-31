b316ae20a58842b3193df0ae512be2e2df79d71d
Jaquarh <okaydots@gmail.com>
2019-03-31 20:27:33 +0100

    Added basic cinematic script

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/login/newplayer.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/login/newplayer.kts
@@ -1,4 +1,5 @@
 import gg.rsmod.game.model.attr.NEW_ACCOUNT_ATTR
+import gg.rsmod.plugins.content.magic.teleport
 
 /**
  * @author Kye <okaydots@gmail.com>
@@ -45,7 +46,27 @@ suspend fun tour(it: QueueTask) {
     it.chatNpc("${world.gameContext.name} is a PvM and PvP dedicated server.", animation = 568, npc = npcId, title = "${world.gameContext.name} Guide")
     it.chatNpc("Making money is simple. You can raid bosses, or, choose to raid players.", animation = 569, npc = npcId, title = "${world.gameContext.name} Guide")
     it.chatPlayer("And where can I start?", animation = 554)
-    it.chatNpc("There are plenty of ways you can skill and gain XP.", animation = 569, npc = npcId, title = "${world.gameContext.name} Guide")
+
+    /** Varrock **/
+    it.player.moveTo(3210, 3424, 0)
+    it.chatNpc("Ah, Varrock. The land of the Grand Exchange.<br>Here, you can sell and buy items between players.", animation = 569, npc = npcId, title = "${world.gameContext.name} Guide")
+    it.chatPlayer("What items are sold here?", animation = 554)
+    it.chatNpc("All types of tradable items can be found at the GE. Pvm/PvP will get you items to sell.", animation = 568, npc = npcId, title = "${world.gameContext.name} Guide")
+
+    /** Lumbridge **/
+    it.player.moveTo(3222, 3218, 0)
+    it.chatNpc("Next, Lumbridge. The land of the Wise, cows and creepy Goblins.", animation = 569, npc = npcId, title = "${world.gameContext.name} Guide")
+    it.chatPlayer("Is this a good area to train?", animation = 554)
+    it.chatNpc("Yes! You can find all of the trainers here: Goblins and Cows are also Lvl 2.", animation = 568, npc = npcId, title = "${world.gameContext.name} Guide")
+
+    /** Duel Arena **/
+    it.player.moveTo(3360 ,3213 , 0)
+    it.chatNpc("Finally, Mini-Games. Welcome to the Duel Arena!", animation = 569, npc = npcId, title = "${world.gameContext.name} Guide")
+    it.chatPlayer("Do I keep my items on death?", animation = 554)
+    it.chatNpc("You can stake your items on a player duel which will be lost if you die.<br>All unstaked items will be kept on death.", animation = 568, npc = npcId, title = "${world.gameContext.name} Guide")
+
+    /** Move Back Home **/
+    it.player.moveTo(3087, 3497, 0)
 }
 
 suspend fun startItems(it: QueueTask) {
