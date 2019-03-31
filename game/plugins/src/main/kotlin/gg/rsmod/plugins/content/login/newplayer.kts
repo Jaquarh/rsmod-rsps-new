3b071f16db1b4eee659313585bb68f156d83a9ed
Jaquarh <okaydots@gmail.com>
2019-03-31 19:43:48 +0100

    Added ability to disabled starter items

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/login/newplayer.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/login/newplayer.kts
@@ -13,7 +13,7 @@ on_login {
 }
 
 suspend fun dialog(it: QueueTask) {
-    it.chatNpc("Good day! Welcome to ${world.gameContext.name}.", animation = 568, npc = npcId, title = "${world.gameContext.name} Guide")
+    it.chatNpc("Good day! Welcome to ${world.gameContext.name}. I'm your guide for today.", animation = 568, npc = npcId, title = "${world.gameContext.name} Guide")
     it.chatNpc("Would you like to take a tour?", animation = 554, npc = npcId, title = "${world.gameContext.name} Guide")
     when (it.options("Yes. A tour would be good.", "No. I am an experienced player.")) {
         1 -> {
