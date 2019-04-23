b512eca06a6084a993b5bf61e0534d3d90ba7b34
Jaquarh <okaydots@gmail.com>
2019-04-23 12:26:33 +0100

    Fixes [PlayerSaveController.savePlayer] Added upsert ability

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/service/sql/controllers/PlayerSaveController.kt
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/service/sql/controllers/PlayerSaveController.kt
@@ -5,6 +5,7 @@ import gg.rsmod.game.model.World
 import gg.rsmod.game.model.entity.Client
 import gg.rsmod.game.model.priv.Privilege
 import gg.rsmod.plugins.service.sql.models.*
+import mu.KLogging
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 
@@ -12,8 +13,13 @@ class PlayerSaveController : Controller() {
     fun savePlayer(client: Client, world: World) {
         transaction {
 
-            // Update  player model
-            PlayerModel.update ({
+            /*
+             * Update [PlayerModel] for player
+             *
+             * We know that all the player data already exist in the DB
+             * since they was created on [PlayerLoadResponse.NEW_ACCOUNT]
+             */
+            PlayerModel.update({
                 PlayerModel.id eq Integer.parseInt(client.uid.value.toString())
             }) {
                 it[username] = client.loginUsername
@@ -31,7 +37,14 @@ class PlayerSaveController : Controller() {
                 it[displayMode] = client.interfaces.displayMode.id
             }
 
-            // Update player skills
+            log("${client.loginUsername}:PlayerModel been updated.")
+
+            /*
+             * Update [SkillModel] for player
+             *
+             * We know that all the skills already exist in the DB
+             * since they was created on [PlayerLoadResponse.NEW_ACCOUNT]
+             */
             SkillModel.select {
                 SkillModel.playerId eq Integer.parseInt(client.uid.value.toString())
             }.forEach { skill ->
@@ -44,69 +57,202 @@ class PlayerSaveController : Controller() {
                 }
             }
 
-            // Save item containers
-            getContainers(client).forEach { container ->
-                val containerKey = ItemContainerModel.select {
+            log("${client.loginUsername}:SkillModel been updated.")
+
+            /*
+             * Update [ItemModel] (has many) for player
+             *
+             * Since the items are not created by default
+             * the index may not yet exist for the [ItemModel]
+             * thus we need to first check it exists before updating it.
+             * If it does not exist, we need to insert a new entry.
+             */
+        }
+
+        getContainers(client).forEach { container ->
+            val containerKey = transaction {
+                ItemContainerModel.select {
                     ItemContainerModel.playerId eq Integer.parseInt(client.uid.value.toString())
                     ItemContainerModel.name eq container.name
-                }.first()[ItemContainerModel.id]
+                }.firstOrNull()
+            }?.get(ItemContainerModel.id) ?: transaction {
+                ItemContainerModel.insert {
+                    it[this.name] = container.name
+                    it[this.playerId] = Integer.parseInt(client.uid.value.toString())
+                } get ItemContainerModel.id
+            }
 
-                container.items.forEach { item ->
+            /*
+             * With the new or existing container key
+             * we can now start inserting or updating the [ItemModel]
+             * based on whether the index of the [Item] exists or not.
+             */
+
+            container.items.forEach { item ->
+                val itemKey = transaction {
+                    ItemModel.select {
+                        ItemModel.containerId eq containerKey
+                        ItemModel.index eq item.key
+                    }.firstOrNull()
+                }
+
+                if (itemKey != null) {
                     ItemModel.update({
+                        ItemModel.index eq itemKey[ItemModel.index]
                         ItemModel.containerId eq containerKey
                     }) {
+                        it[itemId] = item.value.id
+                        it[amount] = item.value.amount
+                    }
+                } else {
+                    ItemModel.insert {
                         it[index] = item.key
                         it[itemId] = item.value.id
                         it[amount] = item.value.amount
-
-                        // Store item attributes
-                        item.value.attr.forEach { attr ->
-                            ItemAttributeModel.update({
-                                ItemAttributeModel.itemId eq item.value.id
-                                ItemAttributeModel.playerId eq Integer.parseInt(client.uid.value.toString())
-                                ItemAttributeModel.key eq attr.key.name
-                            }) { upd ->
-                                upd[value] = attr.value.toString()
-                            }
-                        }
                     }
                 }
-            }
 
-            // Save attributes
-            client.attr.toPersistentMap().forEach { attr ->
-                AttributesModel.update({
+                /*
+                 * We need to ensure the [ItemAttributeModel] exists
+                 * before updating it. If it doesn't exist, we can
+                 * insert a new row.
+                 */
+
+                // Store item attributes
+                item.value.attr.forEach { attr ->
+                    val attrKey = transaction {
+                        ItemAttributeModel.select {
+                            ItemAttributeModel.itemId eq item.value.id
+                            ItemAttributeModel.playerId eq Integer.parseInt(client.uid.value.toString())
+                            ItemAttributeModel.key eq attr.key.name
+                        }.firstOrNull()
+                    }
+
+                    if (attrKey != null) {
+                        ItemAttributeModel.update({
+                            ItemAttributeModel.id eq attrKey[ItemAttributeModel.id]
+                        }) { upd ->
+                            upd[value] = attr.value.toString()
+                        }
+                    } else {
+                        ItemAttributeModel.insert {
+                            it[this.itemId] = item.value.id
+                            it[this.key] = attr.key.name
+                            it[this.playerId] = Integer.parseInt(client.uid.value.toString())
+                            it[this.value] = attr.value.toString()
+                        }
+                    }
+
+                    log("${client.loginUsername}:ItemModel been updated.")
+                }
+            }
+        }
+
+        log("${client.loginUsername}:ItemContainerModel been updated.")
+
+        /*
+        * Update [AttriubtesModel] (has many) for player
+        *
+        * Since [AttributesModel] is not inserted by default
+        * we must check it exists before updating.
+        * If it doesn't exist, we insert a new row.
+        */
+        client.attr.toPersistentMap().forEach { attr ->
+            val attrKey = transaction {
+                AttributesModel.select {
                     AttributesModel.playerId eq Integer.parseInt(client.uid.value.toString())
                     AttributesModel.key eq attr.key
+                }.firstOrNull()
+            }
+
+            if(attrKey != null) {
+                AttributesModel.update({
+                    AttributesModel.id eq attrKey[AttributesModel.id]
                 }) {
                     it[value] = attr.value.toString()
                 }
+            } else {
+                AttributesModel.insert {
+                    it[this.key] = attr.key
+                    it[this.playerId] = Integer.parseInt(client.uid.value.toString())
+                    it[this.value] = attr.value.toString()
+                }
             }
+        }
 
-            // Save timers
-            client.timers.toPersistentTimers().forEach { timer ->
-                if(timer.identifier != null) {
-                    TimerModel.update({
+        log("${client.loginUsername}:AttributeModel been updated.")
+
+        /*
+        * Update [TimerModel] (has many) for player
+        *
+        * Since [TimerModel] is not inserted by default
+        * we must check it exists before updating.
+        * If it doesn't exist, we insert a new row.
+        */
+        client.timers.toPersistentTimers().forEach { timer ->
+            if(timer.identifier != null) {
+
+                val timerKey = transaction {
+                    TimerModel.select {
                         TimerModel.playerId eq Integer.parseInt(client.uid.value.toString())
                         TimerModel.identifier eq timer.identifier.toString()
+                    }.firstOrNull()
+                }
+
+                if(timerKey != null) {
+                    TimerModel.update({
+                        TimerModel.id eq timerKey[TimerModel.id]
                     }) {
                         it[currentMs] = timer.currentMs
                         it[tickOffline] = timer.tickOffline
                         it[timeLeft] = timer.timeLeft
                     }
-                }
-            }
-
-            // Save varps
-            client.varps.getAll().forEach { varp ->
-                VarpModel.update({
-                    VarpModel.varpId eq varp.id
-                    VarpModel.playerId eq Integer.parseInt(client.uid.value.toString())
-                }) {
-                    it[state] = varp.state
+                } else {
+                    TimerModel.insert {
+                        it[this.currentMs] = timer.currentMs
+                        it[this.tickOffline] = timer.tickOffline
+                        it[this.timeLeft] = timer.timeLeft
+                        it[this.playerId] = Integer.parseInt(client.uid.value.toString())
+                        it[this.identifier] = timer.identifier.toString()
+                    }
                 }
             }
         }
+
+        log("${client.loginUsername}:TimerModel been updated.")
+
+        /*
+        * Update [VarpModel] (has many) for player
+        *
+        * Since [VarpModel] is not inserted by default
+        * we must check it exists before updating.
+        * If it doesn't exist, we insert a new row.
+        */
+        client.varps.getAll().forEach { varp ->
+
+            val varpKey = transaction {
+                VarpModel.select {
+                    VarpModel.varpId eq varp.id
+                    VarpModel.playerId eq Integer.parseInt(client.uid.value.toString())
+                }.firstOrNull()
+            }
+
+            if(varpKey != null) {
+                VarpModel.update({
+                    VarpModel.id eq varpKey[VarpModel.id]
+                }) {
+                    it[state] = varp.state
+                }
+            } else {
+                VarpModel.insert {
+                    it[this.state] = varp.state
+                    it[this.playerId] = Integer.parseInt(client.uid.value.toString())
+                    it[this.varpId] = varp.id
+                }
+            }
+        }
+
+        log("${client.loginUsername}:VarpModel been updated.")
     }
 
     fun createPlayer(client: Client, world: World): PlayerUID {
@@ -134,11 +280,12 @@ class PlayerSaveController : Controller() {
 
         /*
          * Generate the players [SkillModel]
+         * Generates the players [ItemContainerModel]
          */
 
         transaction {
 
-            // Since there are 22 skills
+            // Since there are n skills
 
             for(i in 0 until client.getSkills().maxSkills) {
                 SkillModel.insert {
@@ -148,8 +295,18 @@ class PlayerSaveController : Controller() {
                 }
             }
 
+            // Lets build the generic inventory
+
+            ItemContainerModel.insert {
+                it[this.name] = "inventory"
+                it[this.playerId] = Integer.parseInt(client.uid.value.toString())
+            }
         }
 
         return PlayerUID(player)
     }
+
+    private fun log(str: String) = logger.info { str }
+
+    companion object: KLogging()
 }
