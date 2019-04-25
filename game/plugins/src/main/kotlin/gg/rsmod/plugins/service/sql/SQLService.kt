e632ca93cf6e7772943d086b73a98c63c393397d
Jaquarh <okaydots@gmail.com>
2019-04-25 12:22:59 +0100

    Adds player loading via SQL

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/service/sql/SQLService.kt
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/service/sql/SQLService.kt
@@ -1,8 +1,16 @@
 package gg.rsmod.plugins.service.sql
 
 import gg.rsmod.game.Server
+import gg.rsmod.game.model.PlayerUID
+import gg.rsmod.game.model.Tile
 import gg.rsmod.game.model.World
+import gg.rsmod.game.model.attr.AttributeKey
+import gg.rsmod.game.model.container.ItemContainer
 import gg.rsmod.game.model.entity.Client
+import gg.rsmod.game.model.interf.DisplayMode
+import gg.rsmod.game.model.item.Item
+import gg.rsmod.game.model.priv.Privilege
+import gg.rsmod.game.model.timer.TimerKey
 import gg.rsmod.game.service.serializer.PlayerLoadResult
 import gg.rsmod.game.service.serializer.PlayerSerializerService
 import gg.rsmod.net.codec.login.LoginRequest
@@ -149,9 +157,68 @@ class SQLService : PlayerSerializerService()
             }
         }
 
-        /*
-         * TODO("Load all data to client")
-         */
+        // Load player into client
+
+        client.uid = PlayerUID(serialize.player[PlayerModel.id])
+        client.passwordHash = serialize.player[PlayerModel.hash]
+        client.username = serialize.player[PlayerModel.displayName]
+        client.runEnergy = serialize.player[PlayerModel.runEnergy].toDouble()
+        client.privilege = client.world.privileges.get(serialize.player[PlayerModel.privilege]) ?: Privilege.DEFAULT
+        client.tile = Tile(serialize.player[PlayerModel.x], serialize.player[PlayerModel.height], serialize.player[PlayerModel.z])
+        client.interfaces.displayMode = DisplayMode.values.firstOrNull { it.id == serialize.player[PlayerModel.displayMode] } ?: DisplayMode.FIXED
+
+        // Load skills into client
+
+        serialize.skillModels.forEach { skill ->
+            client.getSkills().setXp(skill[SkillModel.skill], skill[SkillModel.xp].toDouble())
+            client.getSkills().setCurrentLevel(skill[SkillModel.skill], skill[SkillModel.lvl])
+        }
+
+        // Load item containers into client
+
+        serialize.itemContainers.forEach { container ->
+            val key = client.world.plugins.containerKeys.firstOrNull { other -> other.name == container.container[ItemContainerModel.name] }
+                    ?: return@forEach
+
+            val cont = if (client.containers.containsKey(key)) client.containers[key] else {
+                client.containers[key] = ItemContainer(client.world.definitions, key)
+                client.containers[key]
+            } ?: return@forEach
+
+            container.items.forEach { item ->
+                val i = Item(item.item[ItemModel.itemId], item.item[ItemModel.amount])
+                item.attributes.forEach { attr ->
+                    //i.attr[attr[ItemAttributeModel.key]] = attr[ItemAttributeModel.value]
+                }
+                cont[item.item[ItemModel.index]] = i
+            }
+        }
+
+        // Load attributes into client
+
+        serialize.attributeModels.forEach { attr ->
+            client.attr[AttributeKey<Any>(attr[AttributesModel.key])] = attr[AttributesModel.value]
+        }
+
+        // Load timers into client
+
+        serialize.timerModels.forEach { timer ->
+            var time = timer[TimerModel.timeLeft]
+            val tick = timer[TimerModel.tickOffline]
+            if(tick) {
+                val elapsed = System.currentTimeMillis() - timer[TimerModel.currentMs]
+                val ticks = (elapsed / client.world.gameContext.cycleTime).toInt()
+                time -= ticks
+            }
+
+            client.timers[TimerKey(timer[TimerModel.identifier], tick)] = Math.max(0, time)
+        }
+
+        // Load varps into client
+
+        serialize.varpModels.forEach { varp ->
+            client.varps.setState(varp[VarpModel.varpId], varp[VarpModel.state])
+        }
 
         return PlayerLoadResult.LOAD_ACCOUNT
     }
